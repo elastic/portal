96511af2bfae4fd584ce7d4d254304da7fdc13ca
Kellen <9484709+goodroot@users.noreply.github.com>
2021-09-08 13:36:29 -0700

    Chyea

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,6 @@ then
   return -1
 fi
 
-if [ -z "$INPUT_DESTINATION_BRANCH" ]
-then
-  INPUT_DESTINATION_BRANCH=main
-fi
 OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH"
 
 CLONE_DIR=$(mktemp -d)
@@ -20,6 +16,7 @@ CLONE_DIR=$(mktemp -d)
 echo "Cloning destination git repository"
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
+git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
 
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
@@ -27,9 +24,6 @@ echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cp -R $INPUT_SOURCE_FILE $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cd "$CLONE_DIR"
-
-git checkout -b "$INPUT_DESTINATION_BRANCH_CREATE"
-OUTPUT_BRANCH="$INPUT_DESTINATION_BRANCH_CREATE"
 
 if [ -z "$INPUT_COMMIT_MESSAGE" ]
 then
